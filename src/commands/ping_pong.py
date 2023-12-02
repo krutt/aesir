b9e9cd84eb612a7130769503b997972ad94009f4
Sitt Guruvanich <aekazitt@gmail.com>
2023-12-02 23:08:24 +0700

    Ignore redefinition of open_channel var

--- src/commands/ping_pong.py
+++ src/commands/ping_pong.py
@@ -103,7 +103,7 @@ def ping_pong(channel_size: int) -> None:
                     errors.append("!! Channel between tranche-ping to tranche-pong already opened.")
             elif container.name == "tranche-pong":
                 try:
-                    open_channel: OpenChannel = TypeAdapter(OpenChannel).validate_json(
+                    open_channel: OpenChannel = TypeAdapter(OpenChannel).validate_json(  # ignore[no-redef]
                         container.exec_run(
                             """
                             lncli
