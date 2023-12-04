98e497c213fb452739fb80280fb185e25415c326
Sitt Guruvanich <aekazitt@gmail.com>
2023-12-05 01:15:18 +0700

    🆙 Version bump

--- src/schemas/cluster_enum.py
+++ src/schemas/cluster_enum.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2022-2023 All rights reserved.
 # FILENAME:    ~~/src/schemas/cluster_enum.py
-# VERSION: 	   0.2.6
+# VERSION: 	   0.2.7
 # CREATED: 	   2023-12-01 05:31
 # AUTHOR: 	   Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION:
