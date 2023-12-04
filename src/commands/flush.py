875f461211207129066a3c67d2244d35f9042a82
Sitt Guruvanich <aekazitt@gmail.com>
2023-12-05 02:38:52 +0700

    Flatten "flush" command

--- src/commands/flush.py
+++ src/commands/flush.py
@@ -16,6 +16,8 @@ from typing import List, Set
 ### Third-party packages ###
 from click import command
 from docker import DockerClient, from_env
+from docker.errors import DockerException
+from rich import print as rich_print
 from rich.progress import track
 
 ### Local modules ###
@@ -25,17 +27,19 @@ from src.configs import DEPRECATED
 @command
 def flush() -> None:
     """Remove images deprecated by workspace."""
-    client: DockerClient = from_env()
-    if client.ping():
-        outputs: List[str] = []
-        docker_images: Set[str] = {image.tags[0] for image in client.images.list()}
-        for registry_id in track(DEPRECATED, "Remove deprecated images:".ljust(42)):
-            if registry_id in docker_images:
-                client.images.remove(registry_id)
-                outputs.append(f"<Image: '{ registry_id }'> removed.")
-        list(map(print, outputs))
-    else:
-        print("!! Unable to connect to docker daemon.")
+    try:
+        client: DockerClient = from_env()
+        if not client.ping():
+            raise DockerException
+    except DockerException:
+        rich_print("[red bold]Unable to connect to docker daemon.")
+    outputs: List[str] = []
+    docker_images: Set[str] = {image.tags[0] for image in client.images.list()}
+    for registry_id in track(DEPRECATED, "Remove deprecated images:".ljust(42)):
+        if registry_id in docker_images:
+            client.images.remove(registry_id)
+            outputs.append(f"<Image: '{ registry_id }'> removed.")
+    list(map(rich_print, outputs))
 
 
 __all__ = ["flush"]
