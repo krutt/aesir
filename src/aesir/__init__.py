56c9f02a7f9cb0b4fee1a1c001e2ed5779e174a1
Sitt Guruvanich <aekazitt@gmail.com>
2024-08-06 13:35:55 +0700

    feat(version): bump 0.4.5 -> 0.4.6

--- src/aesir/__init__.py
+++ src/aesir/__init__.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2022-2024 All rights reserved.
 # FILENAME:    ~~/src/aesir/__init__.py
-# VERSION:     0.4.5
+# VERSION:     0.4.6
 # CREATED:     2023-12-01 05:31
 # AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
@@ -15,4 +15,4 @@ from aesir.commands import build, clean, deploy, flush, mine, nodekeys, ping_pon
 
 __all__ = ("build", "clean", "deploy", "flush", "mine", "nodekeys", "ping_pong", "pull")
 
-__version__ = "0.4.5"
+__version__ = "0.4.6"
--- src/aesir/types/peripheral_enum.py
+++ src/aesir/types/peripheral_enum.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2022-2024 All rights reserved.
 # FILENAME:    ~~/src/aesir/types/peripheral_enum.py
-# VERSION:     0.4.5
+# VERSION:     0.4.6
 # CREATED:     2023-12-03 01:11
 # AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION:
