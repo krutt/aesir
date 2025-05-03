9df9dfa47ace23a406ce366dc6fbc9e56b8c0d5a
Sitt Guruvanich <aekazitt@gmail.com>
2025-05-03 17:03:08 +0700

    chore: bump version; 0.5.1 -> 0.5.2

--- src/aesir/commands/deploy.py
+++ src/aesir/commands/deploy.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2022-2025 All rights reserved.
 # FILENAME:    ~~/src/aesir/commands/deploy.py
-# VERSION:     0.5.1
+# VERSION:     0.5.2
 # CREATED:     2023-12-01 05:31
 # AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION:
